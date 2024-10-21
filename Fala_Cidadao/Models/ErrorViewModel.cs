48776bfb50c0f03e9a101d2a15750447fdc84b0c
hana karolina <hanakarolina@live.com>
2024-10-21 14:06:30 -0300

    Adicionar arquivos de projeto.

---
+++ Fala_Cidadao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fala_Cidadao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
